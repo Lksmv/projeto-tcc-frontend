e269850d46f485e54d7ec3d4c28a705278d0b443
amandaMZ <amandamirzanella@gmail.com>
2023-09-25 20:56:58 -0300

    Adicionado express

---
+++ server.jsx
@@ -0,0 +1,16 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(dirname + '/build/favicon.ico'));
+// the dirname is the current directory from where the script is running
+app.use(express.static(dirname));
+app.use(express.static(path.join(dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
