7c2ee8e38ed554537a0af12ad79d09fd81971286
Lksmv <lks.migueel@gmail.com>
2023-10-04 21:55:08 -0300

    teste

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve os arquivos estáticos da pasta 'dist' gerada pelo Vite
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Rota principal para servir o HTML
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-// Porta em que o servidor irá ouvir
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@mui/material"
-
-export default function App() {
-  return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
-  )
-}
